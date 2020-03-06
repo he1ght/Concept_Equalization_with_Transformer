a39b7185fe6cc61dca62c29df501e1be564810d9
he1ght <heesng.jung@gmail.com>
2020-03-06 20:35:40 +0900

    Extract Embedder. Multi Task per GPU fix

--- irr_nmt/onmt/temp.py
+++ irr_nmt/onmt/temp.py
@@ -1,26 +1,43 @@
 import torch
-def euclidean_distance(inputs, target):
-    return torch.sqrt(torch.sum((inputs - target) ** 2))
+import gensim
+
+model = gensim.models.KeyedVectors.load_word2vec_format('~/codes/GoogleNews-vectors-negative300.bin', binary=True)
+
+print(model)
+
+weights = torch.FloatTensor(model.vectors)  # formerly syn0, which is soon deprecated
 
 
-def hamming_distance(inputs, target, norm=True):
-    # [n_seq x batch x v_dim]
-    v_dim = inputs.size(-1)
+def from_pretrained(embeddings, freeze=True):
+    assert embeddings.dim() == 2, \
+        'Embeddings parameter is expected to be 2-dimensional'
+    rows, cols = embeddings.shape
+    embedding = torch.nn.Embedding(num_embeddings=rows, embedding_dim=cols)
+    embedding.weight = torch.nn.Parameter(embeddings)
+    embedding.weight.requires_grad = not freeze
+    return embedding
 
-    ret = torch.abs(inputs - target)
-    ret = torch.sum(ret, dim=-1)
-    if norm:
-        ret = ret / v_dim
-    ret = torch.mean(ret)
-    return ret
 
-a = torch.rand((2,3,4))
-b = torch.rand((2,3,4))
+embedding = from_pretrained(weights)
+print(embedding)
+# a = model.word_vec("King")
+# b = model.word_vec("de")
+# c = model.word_vec("européenne")
 
-# print(a)
-# print(b)
+# print(model.vocab.keys())
+print(len(model.vocab.keys()))
 
-# d1 = euclidean_distance(a, b)
-d2 = hamming_distance(a, b)
-# print(d1)
-print(d2)
+# x = torch.Tensor([model.vocab["King"].index]).long()
+# print((embedding(x) - a).sum())
+# print(model.distance("Apple", "Banana"))
+
+# model.vectors[self.vocab[word].index]
+
+
+# with open('../../../../codes/glove_dir/glove.6B.300d.txt', 'r') as f:
+#     a = f.readlines()
+#     lines = []
+#     for line in a:
+#         print(line)
+#         break
+#     print(len(a))
