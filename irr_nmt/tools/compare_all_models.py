3a470b98c5b373978f24b81b2d716e3f62cdc500
he1ght <dkcla0223@gmail.com>
2020-04-23 19:08:13 +0900

    analyzer

--- irr_nmt/tools/compare_all_models.py
+++ irr_nmt/tools/compare_all_models.py
@@ -89,10 +89,10 @@ if __name__ == '__main__':
     better_cnt = len(intersection)
     for idx in intersection:
         print("No. {}".format(idx + 1))
-        print("REF: {}".format(" ".join(list_of_references[idx][0])))
+        print("REF: {:15s}".format(" ".join(list_of_references[idx][0])))
         for i, (hypothesis, hypothesis_ce) in enumerate(zip(list_of_hypothesis, list_of_hypothesis_ce)):
-            print("{}: {}".format(opt.pred[i], " ".join(hypothesis[idx])))
-            print("{}: {}".format(opt.pred_ce[i], " ".join(hypothesis_ce[idx])))
+            print("{:15s}: {}".format(opt.pred[i].split('/')[-1][:-4], " ".join(hypothesis[idx])))
+            print("{:15s}: {}".format(opt.pred_ce[i].split('/')[-1][:-4], " ".join(hypothesis_ce[idx])))
         print()
     print(intersection)
     round_better_score = round(better_cnt/total_cnt, 4) * 100
